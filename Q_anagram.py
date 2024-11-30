a4ce64b4d215866eb44ee21bfecc558c7d0c8ca2
Sabil Firdausi <53112395+firdausi555@users.noreply.github.com>
2024-11-30 12:26:45 +0530

    Create Q_anagram.py

---
+++ Q_anagram.py
@@ -0,0 +1,7 @@
+from collections import Counter 
+class Solution:
+    
+    #Function is to check whether two strings are anagram of each other or not.
+    def areAnagrams(self, s1, s2):
+        #code here
+        return Counter(s1)==Counter(s2)
