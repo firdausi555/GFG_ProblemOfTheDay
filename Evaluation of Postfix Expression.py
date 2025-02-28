d1e094cea1cfd9705ab914566870d29ae71c2188
Sabil Firdausi <53112395+firdausi555@users.noreply.github.com>
2025-02-28 20:58:38 +0530

    Create Evaluation of Postfix Expression.py

---
+++ Evaluation of Postfix Expression.py
@@ -0,0 +1,21 @@
+class Solution:
+    def evaluate(self, arr):
+        stack = []
+        
+        for token in arr:
+            if token in {'+', '-', '*', '/'}:
+                b = stack.pop()
+                a = stack.pop()
+                
+                if token == '+':
+                    stack.append(a + b)
+                elif token == '-':
+                    stack.append(a - b)
+                elif token == '*':
+                    stack.append(a * b)
+                elif token == '/':
+                    stack.append(int(a / b)) 
+            else:
+                stack.append(int(token)) 
+        
+        return stack[0]
