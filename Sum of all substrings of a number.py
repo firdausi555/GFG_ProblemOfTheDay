efdfa8493c9f0b09b42dca359aa0dc83d6310ec6
Sabil Firdausi <53112395+firdausi555@users.noreply.github.com>
2025-05-24 02:33:19 +0530

    Create Sum of all substrings of a number.py

---
+++ Sum of all substrings of a number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sumSubstrings(self, s):
+        n = len(s)
+        # mod = 10**9 + 7  # In case of larger constraints
+        total = 0
+        prev = 0
+
+        for i in range(n):
+            num = int(s[i])
+            curr = (prev * 10 + num * (i + 1)) 
+            total = (total + curr) 
+            prev = curr
